28391487f522276a88b5ca95189c87a066c2ae99
deterquetejoder <saecularisator@gmail.com>
2023-09-13 21:10:27 +0500

    fck dnt wrk

--- ts54/Program.cs
+++ ts54/Program.cs
@@ -1,3 +1,32 @@
 ﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по
 //  убыванию элементы каждой строки двумерного массива.
 
+
+            int[,] array = { 
+                 { 5, 3, 1 },
+                 { 4, 2, 7 },
+                 { 9, 7, 6 },
+            };
+
+            foreach (var row in array)
+            {
+                SortRow(row);
+            }
+
+            PrintArrays(array);
+        
+
+        static void SortRow(int[] row)
+        { 
+            Array.Sort(row); 
+            Array.Reverse(row);  
+        }
+
+
+        static void PrintArrays(int[,] arrays)
+        {  
+            foreach(var array in arrays)
+            {  
+                Console.WriteLine(string.Join(" ", array));
+            }  
+         } 
