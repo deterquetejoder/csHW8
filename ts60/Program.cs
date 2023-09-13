c2ce56643aac115ed26569d3e74f9b134450dd41
deterquetejoder <saecularisator@gmail.com>
2023-09-13 21:58:21 +0500

    this neither

--- ts60/Program.cs
+++ ts60/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[][][] array = new int[2][][];
+
+        for (int i = 0; i < 2; i++)
+        {
+            array[i] = new int[2][];
+
+            for (int j = 0; j < 2; j++)
+            {
+                array[i][j] = new int[10];
+
+                for (int k = 0; k < 10; k++)
+                {
+                    array[i][j][k] = (i * 10) + j + k;
+                }
+            }
+        }
+
+        foreach (int[][] row in array)
+        {
+            Console.WriteLine();
+            foreach (int[] column in row)
+            {
+                foreach (int value in column)
+                {
+                    Console.Write($"{value}, ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
