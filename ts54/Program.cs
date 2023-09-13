5547dc49afcaebec61da76665221c77974e6f943
deterquetejoder <saecularisator@gmail.com>
2023-09-13 21:30:58 +0500

    sht works

--- ts54/Program.cs
+++ ts54/Program.cs
@@ -1,32 +1,49 @@
-﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по
-//  убыванию элементы каждой строки двумерного массива.
+﻿using System;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] array = {
+            { 5, 3, 14 },
+            { 4, 2, 7 },
+            { 2, 7, 10 }
+        };
 
-            int[,] array = { 
-                 { 5, 3, 1 },
-                 { 4, 2, 7 },
-                 { 9, 7, 6 },
-            };
-
-            foreach (var row in array)
-            {
-                SortRow(row);
-            }
-
-            PrintArrays(array);
-        
-
-        static void SortRow(int[] row)
-        { 
-            Array.Sort(row); 
-            Array.Reverse(row);  
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            SortRow(array, i);
         }
 
+        PrintArrays(array);
+    }
 
-        static void PrintArrays(int[,] arrays)
-        {  
-            foreach(var array in arrays)
-            {  
-                Console.WriteLine(string.Join(" ", array));
-            }  
-         } 
+    static void SortRow(int[,] array, int rowIndex)
+    {
+        int[] row = new int[array.GetLength(1)];
+        for (int i = 0; i < array.GetLength(1); i++)
+        {
+            row[i] = array[rowIndex, i];
+        }
+
+        Array.Sort(row);
+        Array.Reverse(row);
+
+        for (int i = 0; i < array.GetLength(1); i++)
+        {
+            array[rowIndex, i] = row[i];
+        }
+    }
+
+    static void PrintArrays(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
